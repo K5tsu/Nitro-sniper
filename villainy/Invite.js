8d80a4e35bca89a9ef4b1b50f2740e15b9e75c18
K5 <jordanjjana@gmail.com>
2023-07-01 18:36:09 +0100

    yh she does

--- villainy/Invite.js
+++ villainy/Invite.js
@@ -15,10 +15,8 @@ module.exports = class Invite {
    }
 
    init() {
-      // Check if the user has it enabled
       if (!settings.invite.enabled) return;
 
-      // Add message handler
       this.client.on('message', this.handleInvite.bind(this));
    }
 
@@ -48,18 +46,14 @@ module.exports = class Invite {
       const location = msg.guild ? `${msg.guild.name} > #${msg.channel.name}` : 'DMs';
       const link = msg.url;
 
-      // Filter out used invites just incase
       invites = invites.filter(i => !this.cache.includes(i));
 
-      // Loop over the invites
       for (const i of invites) {
          const { invite: { delay: { min, max }, queue } } = settings;
 
-         // Max server check
          const maximum = this.client.user.premiumType == 2 ? 200 : 100;
          if (this.client.guilds.cache.size >= maximum) {
             if (queue) {
-               // Get account
                const account = active.find(client => {
                   if (client.user.id == this.client.user.id) return null;
 
@@ -72,7 +66,6 @@ module.exports = class Invite {
                   return client;
                });
 
-               // If account is found, join the invite on that account
                account?.invite?.handleInvite(msg, invites);
             }
 
@@ -80,35 +73,27 @@ module.exports = class Invite {
             break;
          }
 
-         // Wait the delay
          const waited = util.randomInt(min * 1000, max * 1000);
          const timeTook = `${(waited / 1000).toFixed(0)} second(s)`;
          await util.sleep(waited);
          if (this.cooldown && this.cooldown > new Date()) return;
 
-         // Fetch invite information
          const invite = await this.client.user.getInvite(i).catch(() => null);
 
          if (!invite?.approximate_member_count) continue;
 
-         // Push code to cache
          this.cache.push(invite.code);
-
-         // Check member count against min and max settings
          if (
             invite.approximate_member_count <= settings.invite.members.min ||
             invite.approximate_member_count >= settings.invite.members.max
          ) continue;
 
-         // Attempt to join the server
          const joined = await this.client.user.acceptInvite(invite.code).catch((err) => ({
             message: err.message,
             error: true
          }));
 
          if (!joined) return;
-
-         // Alert result
          if (joined.error && joined.message) {
             if (webhook) webhook.fire('inviteFail', {
                invite: invite.code,
@@ -131,7 +116,6 @@ module.exports = class Invite {
             ));
          }
 
-         // Fire webhook
          if (joined) {
             if (webhook) webhook.fire('inviteJoin', {
                invite: invite.code,
@@ -154,7 +138,6 @@ module.exports = class Invite {
          }
       }
 
-      // Check Max Bucket
       if (this.joinedBucket >= this.bucket) {
          let date = new Date();
          date.setHours(date.getHours() + settings.invite.cooldown);
